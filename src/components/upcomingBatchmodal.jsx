e0cfcd6773002362fb6089b054a86a0518e545cf
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-12-26 00:23:23 +0530

    update upcomingBatchmodal.jsx

--- src/components/upcomingBatchmodal.jsx
+++ src/components/upcomingBatchmodal.jsx
@@ -59,7 +59,7 @@ const UpcomingBatchModal = ({ onClose }) => {
             }}
             className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition"
           >
-            Book a seat
+            Book a seatasdf
           </button>
         </div>
 
